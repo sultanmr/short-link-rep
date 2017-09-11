fae3cdb4b7dabad25f3db11e876e86fb1b2f6af3
Sultan <sultanmr@hotmail.com>
2017-09-11 18:53:24 +0200

    updated NotFound component

--- imports/ui/NotFound.js
+++ imports/ui/NotFound.js
@@ -6,7 +6,7 @@ export default () => {
         return (
                 <div className='boxed-view'>
                         <div className='boxed-view__box'>
-                                <h1>Page Not Found</h1>
+                                <h1>404 Page Not Found</h1>
                                 <p>We are unable to find that page</p>
                                 <Link to="/" className='button button--link'>Home</Link>
                         </div></div>
